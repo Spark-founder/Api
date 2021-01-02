8bdc042ef923c5bbb57396795fcef3d8733131d5
Dhruv <spark.founder@gmail.com>
2021-01-03 00:58:02 +0530

    master

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from flask_simple_geoip import SimpleGeoIP
 import flask
 from flask import request, jsonify
 from location import location
 
 app = flask.Flask(__name__)
+
+app.config.update(GEOIPIFY_API_KEY='YOUR_API_KEY')
+simple_geoip = SimpleGeoIP(app)
 
 
 @app.route('/', methods=['GET'])
@@ -11,3 +15,9 @@ def home():
         return jsonify(location())
     except KeyError:
         return 'Failed'
+
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    geoip_data = simple_geoip.get_geoip_data()
+    return jsonify(data=geoip_data)
